5d0b88fef4d05f64106a978f938e23eccbd1b6aa
Meredith Espinosa <goclonefilms@gmail.com>
2018-08-22 08:31:21 -0700

    few patches over here

--- src/main/kotlin/com/example/examplemod/TileEntityExample.kt
+++ src/main/kotlin/com/example/examplemod/TileEntityExample.kt
@@ -1,6 +1,6 @@
-package space.bbkr.zeropoint
+package com.example.examplemod
 
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.tileentity.TileEntityType
 
-class TileEntityExample @JvmOverloads constructor(type: TileEntityType<*> = ExampleMod.EXAMPLE_TE) : TileEntity(type)
+class TileEntityExample constructor(type: TileEntityType<*> = ExampleMod.EXAMPLE_TE) : TileEntity(type)
